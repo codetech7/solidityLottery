7fdde5cfe2445b175beea81cb74e678b1fe58b64
codetech7 <codetechseven@gmail.com>
2022-05-11 23:39:54 +0100

    mocha added to package.json test field

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,11 @@ const assert = require("assert");
 const Web3 = require("web3")
 const ganache = require("ganache");
 
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+}
+);
