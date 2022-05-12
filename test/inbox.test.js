0c773c68bc5fe3abbe012730d9ad0d7b48be6abc
codetech7 <codetechseven@gmail.com>
2022-05-12 11:40:38 +0100

    constructor function working properly

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -6,6 +6,7 @@ const { interface , bytecode} = require("../compile");
 const web3 = new Web3(ganache.provider());
 
 let accounts, inbox;
+const INITIAL_MESSAGE = "Hello World";
 
 beforeEach(async () => {
     //get a list of accounts from ganache
@@ -13,17 +14,18 @@ beforeEach(async () => {
 
     //deploy from the first account gotten
    inbox = await new  web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments:[ "Hello World"]})
+    .deploy({data: bytecode, arguments:[ INITIAL_MESSAGE ]})
     .send({from: accounts[0], gas: 900000}); // returns address of deployed contract on blockchain
 
 }
 );
 
 describe("deploy contract", async () => {
-  it("can deploy", () => {
-    console.log(inbox);
-    console.log(accounts[0]);
-    
+  it("can deploy", async () => {
+    assert.ok(inbox.options.address);
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_MESSAGE);
+
   }) 
   
   
