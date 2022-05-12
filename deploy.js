638c3141cc3db7d9546548281c9e8d27cf4ddcce
codetech7 <codetechseven@gmail.com>
2022-05-12 17:43:52 +0100

    added an encapsulating await function for async

--- deploy.js
+++ deploy.js
@@ -5,10 +5,15 @@ const {bytecode, interface} = require("./compile");
 
 const provider = new HDWallet("coil rain dwarf earn chase acoustic fold drive wonder mountain sock impact", "https://rinkeby.infura.io/v3/4bf2abe843a049e5b784627c536d31b7");
 const web3 = new Web3(provider);
-const account = web3.eth.getAccounts;
 
-inbox =new web3.eth.Contracts(JSON.parse(interface)) //defines interface for contract deployment
+const deploy = async () => {
+  
+
+const account = await web3.eth.getAccounts;
+
+const inbox = await new web3.eth.Contracts(JSON.parse(interface)) //defines interface for contract deployment
 .deploy({data: bytecode, arguments: ["First Deployment"]}) //creates a transaction object to be deployed
 .send({from: account, gas: 1000000}); //sends the transaction object created earlierto the network
+}
 
-
+deploy();
