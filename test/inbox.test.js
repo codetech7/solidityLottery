f83417a662f799bc6461fd2d9f84ee6c3930b083
codetech7 <codetechseven@gmail.com>
2022-05-12 09:23:26 +0100

    finally deployed something

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,20 +1,27 @@
 const assert = require("assert");
 const Web3 = require("web3");
 const ganache = require("ganache-cli");
-const { bytecode } = require("../compile");
+const { interface , bytecode} = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
-let accounts;
+let accounts, inbox;
 
 beforeEach(async () => {
+    //get a list of accounts from ganache
     accounts = await web3.eth.getAccounts();
+
+    //deploy from the first account gotten
+   inbox = await new  web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments:[ "Hello World"]})
+    .send({from: accounts[0], gas: 1000000}); // returns address of deployed contract on blockchain
+
 }
 );
 
 describe("deploy contract", () => {
   it("can deploy", () => {
-    console.log(accounts[0]);
+    console.log(inbox);
   }) 
   
   
