1fa3c54f2ba6ed077317d7fdc81954adb23c618d
codetech7 <codetechseven@gmail.com>
2022-05-12 09:24:07 +0100

    fixed errors with module.exports

--- compile.js
+++ compile.js
@@ -6,4 +6,4 @@ const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
 
-module.exports = console.log(solc.compile(source, 1).contracts[':Inbox']);
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
