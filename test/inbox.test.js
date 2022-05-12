5bb2072ae6d1a107736a7137ad0b3efb551c77a6
codetech7 <codetechseven@gmail.com>
2022-05-12 01:54:34 +0100

    testing list of retrieved accounts

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
-const Web3 = require("web3")
-const ganache = require("ganache");
+const Web3 = require("web3");
+const ganache = require("ganache-cli");
+const { bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
@@ -8,5 +9,15 @@ let accounts;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+}
+);
+
+describe("deploy contract", () => {
+  it("can deploy", () => {
+    console.log(accounts[0]);
+  }) 
+  
+  
+
 }
 );
