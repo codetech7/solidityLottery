ffce0e98f68dfa581bf275edb32bf8013df2cfe6
codetech7 <codetechseven@gmail.com>
2022-05-17 22:59:46 +0100

    installed ganache and set up a test template mocha

---
+++ test/lottery.test.js
@@ -0,0 +1,31 @@
+const assert = require("asert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+const web3 = new Web3(ganache.provider());
+
+beforeEach("testingLevels", () => {
+  
+})
+
+describe("lotteryTests", () => {
+  it("can be created", () => {
+    
+  })
+
+  it("can be entered", () => {
+    
+  })
+
+  it("can collect money", () => {
+    
+  })
+
+  it("can pick a winner", () => {
+    
+  })
+
+  it("can send money to winner", () => {
+    
+  })
+})
