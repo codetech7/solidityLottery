ff3697bda8c6ef7b56e5a7a14979891497c53e43
codetech7 <codetechseven@gmail.com>
2022-05-17 22:28:40 +0100

    added mocha, web3 and solc and rewrite compile

--- compile.js
+++ compile.js
@@ -1,9 +1,10 @@
-const path = require("path");
-const fs = require('fs');
-const solc = require('solc')
+// const path = require("path");
+// const fs = require('fs');
+// const solc = require('solc')
 
-const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf8");
+// const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
+// const source = fs.readFileSync(inboxPath, "utf8");
 
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+// module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
