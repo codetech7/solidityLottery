d75d26a1470e3595aed7f2e6f0e581cc63aa0aba
codetech7 <codetechseven@gmail.com>
2022-05-17 22:46:55 +0100

    retyped the compile code snippet

--- compile.js
+++ compile.js
@@ -8,3 +8,11 @@
 
 // module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf-8");
+
+module.exports = solc.compile(source, 1).contracts[":lottery"]
