79156747926ff68a8e45240a21f8f3992d304bc5
codetech7 <codetechseven@gmail.com>
2022-05-17 23:36:27 +0100

    obtained ganache accounts and deployed contract

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,16 +1,23 @@
-const assert = require("asert");
+const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+const {interface, bytecode} = require("../compile.js");
 
 const web3 = new Web3(ganache.provider());
+let lotteryAccounts, deployment;
 
-beforeEach("testingLevels", () => {
-  
+beforeEach("", async () => {
+ lotteryAccounts = await web3.eth.getAccounts();
+
+  deployment = await new web3.eth.contracts(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({from: lotteryAccounts[0], gas: 1000000});
+ 
 })
 
 describe("lotteryTests", () => {
   it("can be created", () => {
-    
+    console.log(lottery);
   })
 
   it("can be entered", () => {
