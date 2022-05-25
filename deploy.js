158531e8a6c608a7fa926967390cc0d8f43c9ef7
codetech7 <codetechseven@gmail.com>
2022-05-25 22:22:01 +0100

    Made slight changes

--- deploy.js
+++ deploy.js
@@ -12,11 +12,16 @@ const deploy = async () => {
 const account = await web3.eth.getAccounts();
 console.log("attemppting to deploy from:", account[0]); //display which account we are deploying from
 
-const inbox = await new web3.eth.Contract(JSON.parse(interface)) //defines interface for contract deployment
-    .deploy({data: bytecode, arguments : ["Heloo World"]}) //creates a transaction object to be deployed
+const lottery = await new web3.eth.Contract(JSON.parse(interface)) //defines interface for contract deployment
+    .deploy({data: bytecode }) //creates a transaction object to be deployed
     .send({from: account[0], gas: 1000000}); //sends the transaction object created earlierto the network
 
-console.log("contract deployed:", inbox.options.address); //log the address the contract waas deployed to.
+console.log("contract deployed to:", lottery.options.address);
+ //log the address the contract waas deployed to.
+
+ console.log("contract interface:", interface);
+
+ return;
 }
 
 
